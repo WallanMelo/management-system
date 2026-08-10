9c7e5c2fdd9b5ed038400da6d8729cad409c9f42
WallanMelo <walandemelo1@gmail.com>
2026-08-10 11:10:41 -0300

    Correção de caminho no main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,8 +17,8 @@ from app.api.routes import (
 
 
 from fastapi import FastAPI
-from app.db.database import engine  
-from app.db.base import Base  
+from app.database import engine  
+from app.database.base import Base  
 
 # Esta linha instrui o SQLAlchemy a criar todas as tabelas caso não existam
 Base.metadata.create_all(bind=engine)
